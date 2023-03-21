5b0aa90f842d916e38c1f0b67c99e90f526bd9e1
adnane miftah <ultra.saidi6@gmail.com>
2023-03-21 11:09:31 -0700

    yarbi nmot

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,14 @@
 
 /**
-  * _islow -  function that checks for lowercase character.
-  *
+  * _islower -  function that checks for lowercase character.
+  *  @c: char type letter
   * Return: 1 or 0
   */
 
 
 int _islower(int c)
 {
-	if (c > 97 && c < 122)
+	if (c > 97 && c <= 122)
 		return (1);
 	else
 		return (0);
