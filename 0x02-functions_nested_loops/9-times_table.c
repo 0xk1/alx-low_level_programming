355f68c52feb36ea7b955b520b9cd991ed5973a3
adnane miftah <ultra.saidi6@gmail.com>
2023-03-21 14:45:05 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,23 +16,26 @@ void times_table(void)
 		while (j <= 9)
 		{
 			r = i * j;
-			if (j != 0)
-			{
-				if (j <= 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
-			if (r < 10)
-			{
-				_putchar(' ');
-				_putchar(r  + '0');
-			}
-			else
+
+			if (r > 9)
 			{
 				_putchar(r / 10 + '0');
 				_putchar(r % 10 + '0');
+			}
+			else if (j != 0)
+			{
+				_putchar(' ');
+				_putchar(r + '0');
+			}
+			else
+			{
+				_putchar(r + '0');
+			}
+
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
 			}
 			j++;
 		}
