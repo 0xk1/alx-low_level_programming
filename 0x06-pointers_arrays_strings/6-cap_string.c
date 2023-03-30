ec6db390c93ec180289111a819bcbece79085429
adnane miftah <ultra.saidi6@gmail.com>
2023-03-30 09:32:14 -0700

    cap string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * cap_string - Captilizes the string given as parameter
+ * @arr: the string
+ * Return: the captilized string
+ **/
+
+char *cap_string(char *arr)
+{
+	int i = 0;
+
+	while (arr[i])
+	{
+		if ((i == 0 || arr[i - 1] == ' ') && (arr[i] >= 'a' && arr[i] <= 'z'))
+		{
+			arr[i] -= 32;
+		}
+		i++;
+	}
+
+	return (arr);
+}
