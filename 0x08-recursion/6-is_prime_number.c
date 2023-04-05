499fb6d47c99e0056c939b6cbd9ee0e1e5f8d5dd
adnane miftah <ultra.saidi6@gmail.com>
2023-04-05 10:44:10 -0700

    prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+int _sqrt(int n, int i);
+
+/**
+ * is_prime_number - returns 1 if prime and 0 if not
+ * @n: the number
+ * Return: 1 or 0
+ */
+int is_prime_number(int n)
+{
+	if (n <= 0)
+		return (0);
+	return (_sqrt(n, (n / 2) + 1));
+
+}
+
+/**
+ * _sqrt - helper function
+ * @n: the number
+ * @i: mid
+ * Return: 1 or 0
+ */
+int _sqrt(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	return (_sqrt(n, i - 1));
+}
