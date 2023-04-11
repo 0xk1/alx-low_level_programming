4342ce90886f24ed4c48e1e9ea71ae8d1aedc111
adnane miftah <ultra.saidi6@gmail.com>
2023-04-11 16:31:07 -0700

    str_concat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+
+
+int _strlen(char *s)
+{
+	int i = 0;
+	while (*(s + i))
+		i++;
+	return (i);
+}
+
+
+char *str_concat(char *s1, char *s2)
+{
+	char *p;
+	int len;
+	int i = 0;
+	int j = 0;
+
+	len = _strlen(s1) + _strlen(s2);
+
+	p = malloc(len + 1);
+	
+	if (!p)
+		return (NULL);
+
+	while (*(s1 +i))
+	{
+		*(p + i) =  *(s1 + i);
+		i++;
+	}
+
+	while (*(s2 + j))
+	{
+		*(p + i) = *(s2 + j);
+		i++;
+		j++;
+	}
+
+	return (p);
+		
+
+
+
+}
