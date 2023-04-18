8dacfc0ce5190f649f84eabc39eaae8e3664f76f
adnane miftah <ultra.saidi6@gmail.com>
2023-04-17 17:26:31 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's onwer
+ * Description: dog desc
+ */
 struct dog
 {
 	char *name;
 	int age;
 	char *owner;
 }
+
+
+#endif
