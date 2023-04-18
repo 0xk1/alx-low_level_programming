9a9f00f83fbeb216cbfd406c491990a41ca0161c
adnane miftah <ultra.saidi6@gmail.com>
2023-04-17 18:02:58 -0700

    task2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_dog - print_dog
+ * @d: struct dog
+ */
+
+void print_dog(struct dog *d)
+{
+	if (!d)
+		return;
+
+	if (!d->name)
+		d->name = "(nil)";
+
+	if (!d-owner)
+		d->owner = "(nil)";
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d-owner);
+}
