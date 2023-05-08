9fb21d5e05745dbba820bddef620988b749b5029
0xk1 <ultra.saidi6@gmail.com>
2023-05-08 13:39:55 +0100

    0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * read_textfile - function that reads a text file and prints it to the POSIX
+ * standard output
+ * @filename: filname
+ * @letters: the number of letters is should read and print
+ * Return: number of letters it could read and print
+ */
+
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t o, r, w;
+	char *buffer;
+
+	if (!filename)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+
+	if (!buffer)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		return (0);
+	}
+	free(buffer);
+	close(0);
+
+	return (w);
+}
